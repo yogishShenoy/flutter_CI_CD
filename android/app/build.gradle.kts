554c3c303cd3d27001e1826dba1bfcac9d2e9485
yogishShenoy <yogishshenoy027@gmail.com>
2026-02-15 20:24:59 +0530

    sdk version changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 34
         versionCode = propOrEnvVersion("APP_VERSION_CODE")?.toInt() ?: 1
         versionName = propOrEnvVersion("APP_VERSION_NAME") ?: "1.0.0"
         logConfig("APP_VERSION_CODE", versionCode)
